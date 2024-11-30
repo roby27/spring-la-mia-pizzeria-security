4bcac0637e1a1dd776b8ae080e2ac70368bd5721
Roberto Branni <rob.branni@gmail.com>
2024-11-30 18:29:44 +0100

    AUTH

--- src/main/java/it/r27/pizzeria/controller/PizzaController.java
+++ src/main/java/it/r27/pizzeria/controller/PizzaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -35,7 +36,7 @@ public class PizzaController {
     private IngredientRepository ingredientRepo;
 
     @GetMapping
-	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+	public String index(Authentication authentication, Model model, @RequestParam(name = "keyword", required = false) String keyword) {
 		
 		List<Pizza> allPizza;
 		
